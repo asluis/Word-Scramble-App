dc0c3307c83475ec0c807c705dd58967b67d0576
asluis <asluisinfo@gmail.com>
2020-12-18 10:53:35 -0800

    File upload

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble App
+//
+//  Created by Luis Alvarez on 12/17/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WordScramble_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScramble_AppApp.swift
+//  WordScramble App
+//
+//  Created by Luis Alvarez on 12/17/20.
+//
+
+import SwiftUI
+
+@main
+struct WordScramble_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
