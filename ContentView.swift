1a3d6b82e6f35f31eb6daec853ddd38be4f96ba5
asluis <asluisinfo@gmail.com>
2020-12-19 12:44:24 -0800

    Can now connect with WordBank and extract words from it

--- ContentView.swift
+++ ContentView.swift
@@ -28,6 +28,9 @@ struct ContentView: View {
                 }
             }
             .navigationTitle(Text("\(rootWord)"))
+            .onAppear{
+                startGame()
+            }
             .navigationBarItems(trailing:
                 Button(action: {
                     print("Button pressed")
@@ -42,6 +45,34 @@ struct ContentView: View {
                 }
             )
         }
+    }
+    
+    func startGame(){
+        /*
+         Starts game, finds and loads WordBank.txt
+         Converts words in .txt to an array of strings,
+         then grabs a random element from array as chosen word.
+         */
+        
+        // Finding WordBank in bundle.main.url
+        if let wordBankURL = Bundle.main.url(forResource: "WordBank", withExtension: ".txt"){
+            
+            //Load WordBank into string format
+            if let words = try? String(contentsOf: wordBankURL){
+                
+                //Split word bank into an array using \n as delimiter
+                let wordBank = words.components(separatedBy: "\n")
+                
+                //Set the rootword to a random word from word bank or default if not possible
+                self.rootWord = wordBank.randomElement() ?? "Default"
+                
+                //Exit
+                return
+            }
+        }
+        
+        //If we make it here then there was an error
+        fatalError("Could not load WordBank.txt")
     }
     
     func addNewWord(){
